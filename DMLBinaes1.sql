6e901d59a9b4f3272a674b21243d54d869f0e46c
AlejoValle <00139121@uca.edu.sv>
2022-06-23 16:29:56 -0600

    commit clase

--- DMLBinaes1.sql
+++ DMLBinaes1.sql
@@ -11,7 +11,7 @@ GO
 insert into EDITORIAL (id, nombre) values (1, 'Santillana');
 GO
 insert into EJEMPLAR (id,Nombre,fecha_publicacion,id_idioma,id_formato,id_palabra_clave,id_editorial) 
-values (1,'Camasutra',CONVERT (DATE, '08/12/2001',103),2,1,1);
+values (1,'Camasutra',CONVERT (DATE, '08/12/2001',103),2,1,1,1);
 GO
 insert into TIPO (id,tipo) values (1,'libro');
 GO
@@ -19,9 +19,10 @@ insert into GENERO (id,genero,id_coleccion) values (1,'sexual',1);
 GO
 insert into COLECCION (id,nombre_coleccion,id_tipo,id_genero,id_ejemplar) values (1,'donaciones +18',1,1,1);
 GO
-insert into MATERIAL_BIBLIOGRAFICO (id,tipo,id_coleccion) values (1,'simon',1);
+insert into MATERIAL_BIBLIOGRAFICO (id,id_coleccion) values (1,1);
 GO
 insert into PISO (id,piso) values (1,1);
 GO
 insert into AREA (id,nombre,descripcion,id_piso,id_material_bibliografico) values (1,'biblioteca','descripcion',1,1);
+
 
