2d50a953cc69c726a5d951fa063dd9f14bff5fcb
AlejoValle <00139121@uca.edu.sv>
2022-06-23 16:28:34 -0600

    clase commit

--- consultasBinaes.sql
+++ consultasBinaes.sql
@@ -1,16 +1,17 @@
+USE BINAES
 
-select Ej.Nombre as 'libro',fo.formato,pc.palabra_clave,ed.nombre as 'editorial',co.nombre_coleccion
-from EJEMPLAR Ej
+SELECT Ej.Nombre as 'libro',fo.formato,pc.palabra_clave,ed.nombre as 'editorial',co.nombre_coleccion
+FROM EJEMPLAR Ej
 INNER JOIN IDIOMA id 
-on ej.id_idioma=id.id
+ON ej.id_idioma=id.id
 INNER JOIN FORMATO fo 
-on fo.id=ej.id_formato
+ON fo.id=ej.id_formato
 INNER JOIN PALABRAS_CLAVE pc
 ON pc.id=Ej.id_palabra_clave
 INNER JOIN EDITORIAL ed 
 ON ed.id=Ej.id_editorial
 INNER join COLECCION co 
-on co.id_ejemplar=Ej.id
+ON co.id_ejemplar=Ej.id
 
-DROP DATABASE BINAES
+
 
