30f742b25079cf5f0b35dbbc9820b24bba6cc3dd
José Alejandro Ramírez Valle <00139121@uca.edu.sv>
2022-06-24 22:51:13 -0600

    commit consultas chidas

--- consultasBinaes.sql
+++ consultasBinaes.sql
@@ -1,6 +1,6 @@
 USE BINAES
 GO
-SELECT Ej.Nombre as 'libro',fo.formato,pc.palabra_clave,ed.nombre as 'editorial',co.nombre_coleccion,Ge.genero
+SELECT Ej.Nombre as 'libro',fo.formato,pc.palabra_clave,ed.nombre as 'editorial',et.etiqueta ,au.nombre as 'autor' ,col.nombre_coleccion,gen.genero
 FROM EJEMPLAR Ej
 INNER JOIN IDIOMA id 
 ON ej.id_idioma=id.id
@@ -10,17 +10,16 @@ INNER JOIN PALABRAS_CLAVE pc
 ON pc.id=Ej.id_palabra_clave
 INNER JOIN EDITORIAL ed 
 ON ed.id=Ej.id_editorial
-INNER join COLECCION co 
-ON co.id_ejemplar=Ej.id
-INNER JOIN GENERO Ge 
-on co.id_genero=Ge.id
+INNER JOIN ETIQUETA et 
+ON et.id=Ej.id_etiqueta
+INNER JOIN AUTOR au
+ON au.id=Ej.id_autor
+INNER JOIN COLECCION col 
+ON col.id=Ej.id_coleccion
+INNER JOIN GENERO gen 
+ON gen.id=col.id_genero
 
 
-SELECT cl.nombre_usuario , cl.Nombre , co.Nombre as Ocupacion FROM CLIENTE cl 
-INNER JOIN INSTITUCION ins 
-ON cl.id_Institucion=ins.id
-INNER JOIN OCUPACION co 
-ON co.id=cl.id_Ocupacion
-WHERE co.id=0006211                                                                                             
+                                                                                         
 
 
